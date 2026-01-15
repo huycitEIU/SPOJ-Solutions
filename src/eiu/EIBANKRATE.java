e0c8fccb6494b3e29a30f8f819d9cd18214aa81d
huy <trangiahuycit@gmail.com>
2026-01-15 15:25:07 +0700

    Add solution: EIBANKRATE

---
+++ src/eiu/EIBANKRATE.java
@@ -0,0 +1,114 @@
+package eiu;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class EIBANKRATE {
+
+    static class FastIO {
+        private InputStream is = System.in;
+        private byte[] inbuf = new byte[1 << 24]; // 16MB buffer
+        private int lenbuf = 0, ptrbuf = 0;
+
+        private int readByte() {
+            if (lenbuf == -1) throw new InputMismatchException();
+            if (ptrbuf >= lenbuf) {
+                ptrbuf = 0;
+                try {
+                    lenbuf = is.read(inbuf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (lenbuf <= 0) return -1;
+            }
+            return inbuf[ptrbuf++];
+        }
+
+        private boolean isWhitespace(int c) {
+            return !(c >= 33 && c <= 126);
+        }
+
+        private int skip() {
+            int b;
+            while ((b = readByte()) != -1 && isWhitespace(b));
+            return b;
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public char nextChar() {
+            return (char) skip();
+        }
+
+        public String next() {
+            int b = skip();
+            StringBuilder sb = new StringBuilder();
+            while (!(isWhitespace(b))) {
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+
+        public char[] nextCharArray(int n) {
+            char[] buf = new char[n];
+            int b = skip(), p = 0;
+            while (p < n && !(isWhitespace(b))) {
+                buf[p++] = (char) b;
+                b = readByte();
+            }
+            return n == p ? buf : Arrays.copyOf(buf, p);
+        }
+
+        public int nextInt() {
+            int num = 0, b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+
+        public long nextLong() {
+            long num = 0;
+            int b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        FastIO fastIO = new FastIO();
+
+        double basedMoney = fastIO.nextDouble();
+        int months = fastIO.nextInt();
+
+        System.out.println(basedMoney + basedMoney * 0.0075 * months);
+    }
+}
